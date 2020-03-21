ba87a3b9c1391c9fbd3fba69cd95afb5e687e999
Rafael Bar <rafaelbar37>
2020-03-21 18:52:41 +0200

    Adding quotes with custome events
    passing add new quote event to the parent app.veu

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,35 @@
 <template>
-  <div id="app">
-   
+  <div class="container">
+    <app-new-quote @quoteAdded="NewQuote"></app-new-quote>
+    <appQuoteGrid :quotes="quotes"></appQuoteGrid>
   </div>
 </template>
 
 <script>
+import QuoteGrid from './components/QuoteGrid.vue';
+import NewQuote from './components/NewQuote.vue';
+
 export default {
   data: function() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      quotes:[
+        'Just a Quote to see something'
+      ],
+      maxQuotes: 10
     }
+  },
+  methods: {
+    NewQuote(quote){
+      this.quotes.push(quote);
+    }
+  },
+  components: {
+    appQuoteGrid: QuoteGrid,
+    appNewQuote: NewQuote
   }
 }
 </script>
 
 <style lang="scss" scoped>
-
+  
 </style>
---
+++ src/components/Quote.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="col-sm-6 col-md-4">
+        <div class="panel panel-default">
+            <div class="panel-body quote">
+                <slot></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+   
+}
+</script>
+
+<style lang="scss" scoped>
+    .panel-body{
+        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+        font-size: 24px;
+        color: #6e6e6e;
+        &.quote{
+            cursor: pointer;
+            &:hover{
+                background-color: #ffe2e2;
+            }
+        }
+    } 
+</style>
